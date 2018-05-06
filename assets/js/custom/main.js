cc9e255dd09fd9bbec47d11f9d09c8c87ed64d31
Muhammad Osama Arshad <osama.arshad@arbisoft.com>
2018-05-07 00:10:09 +0500

    Improve: final fixes

--- assets/js/custom/main.js
+++ assets/js/custom/main.js
@@ -1,13 +1,61 @@
 (function($) {
-    "use strict";
-    var chartCanvas = document.getElementById("pcq-chart");
-    var ctx = chartCanvas.getContext("2d");
+  "use strict";
+  var chartCanvas = document.getElementById("pcq-chart");
+  var ctx = chartCanvas.getContext("2d");
 
-    var $steps = $(".step");
-    var currentStep = 0;
-    $steps.addClass("hidden");
+  var $steps = $(".step");
+  var currentStep = 0;
+  $steps.addClass("hidden");
 
-    var colorPresets = [
+  var colorPresets = [
+    "rgba(184, 14, 1, 0.5)",
+    "rgba(255, 132, 29, 0.5)",
+    "rgba(234, 212, 37, 0.5)",
+    "rgba(13, 172, 64, 0.5)",
+    "rgba(22, 125, 238, 0.5)",
+    "rgba(93, 33, 143, 0.5)",
+    "rgba(234, 94, 150, 0.5)"
+  ];
+
+  $("input:radio").on("click", function() {
+    var $this = $(this);
+
+    $this
+      .parents(".select-box")
+      .siblings(".select-box")
+      .removeClass("active");
+    $this.parents(".select-box").addClass("active");
+  });
+
+  $(document).ready(function() {
+    $($steps[currentStep]).removeClass("hidden");
+
+    $(".step-butn").on("click", function(evt) {
+      evt.preventDefault();
+
+      showNextStep();
+    });
+
+    $(".select-box > input").on("click", function() {
+      var $this = $(this);
+
+      addDatatToChart($this.val(), $this.attr("name"));
+      showNextStep();
+    });
+
+    $(".select-box > span").on("click", function() {
+      $(this)
+        .siblings("input")
+        .trigger("click");
+    });
+
+    $("#restart-butn").on("click", function(evt) {
+      evt.preventDefault();
+      myChart.data.datasets[0].data = [];
+      myChart.data.labels = [];
+
+      myChart.data.datasets[0].backgroundColor = [];
+      colorPresets = [
         "rgba(184, 14, 1, 0.5)",
         "rgba(255, 132, 29, 0.5)",
         "rgba(234, 212, 37, 0.5)",
@@ -15,115 +63,92 @@
         "rgba(22, 125, 238, 0.5)",
         "rgba(93, 33, 143, 0.5)",
         "rgba(234, 94, 150, 0.5)"
-    ];
+      ];
+      myChart.update();
 
-    $("input:radio").on("click", function() {
-        var $this = $(this);
-
-        $this
-            .parents(".select-box")
-            .siblings(".select-box")
-            .removeClass("active");
-        $this.parents(".select-box").addClass("active");
+      $steps.addClass("hidden");
+      currentStep = 0;
+      $($steps[currentStep]).removeClass("hidden");
     });
+  });
 
-    $(document).ready(function() {
-        $($steps[currentStep]).removeClass("hidden");
+  function showNextStep() {
+    $steps.addClass("hidden");
+    currentStep++;
+    $($steps[currentStep]).removeClass("hidden");
+  }
 
-        $(".step-butn").on("click", function(evt) {
-            evt.preventDefault();
+  function showPrevStep() {
+    $steps.addClass("hidden");
+    currentStep--;
+    $($steps[currentStep]).removeClass("hidden");
+  }
 
-            showNextStep();
-        });
+  function createGradient(color) {
+    var xCoor = $(chartCanvas).width() / 2;
+    var yCoor = $(chartCanvas).height() / 2 + 35;
 
-        $(".select-box > input").on("click", function() {
-            var $this = $(this);
+    var gradient = ctx.createRadialGradient(
+      xCoor,
+      yCoor,
+      0.0,
+      xCoor,
+      yCoor,
+      xCoor / 4
+    );
 
-            addDatatToChart($this.val(), $this.attr("name"));
-            showNextStep();
-        });
+    gradient.addColorStop(0.0, "white");
+    gradient.addColorStop(1.0, color);
 
-        $(".select-box > span").on("click", function() {
-            $(this)
-                .siblings("input")
-                .trigger("click");
-        });
-    });
+    return gradient;
+  }
 
-    function showNextStep() {
-        $steps.addClass("hidden");
-        currentStep++;
-        $($steps[currentStep]).removeClass("hidden");
-    }
-
-    function showPrevStep() {
-        $steps.addClass("hidden");
-        currentStep--;
-        $($steps[currentStep]).removeClass("hidden");
-    }
-
-    function createGradient(color) {
-        var xCoor = $(chartCanvas).width() / 2;
-        var yCoor = $(chartCanvas).height() / 2 + 35;
-
-        var gradient = ctx.createRadialGradient(
-            xCoor,
-            yCoor,
-            0.0,
-            xCoor,
-            yCoor,
-            xCoor / 4
-        );
-
-        gradient.addColorStop(0.0, "white");
-        gradient.addColorStop(1.0, color);
-
-        return gradient;
-    }
-
-    var myChart = new Chart(ctx, {
-        type: "polarArea",
-        data: {
-            labels: [],
-            datasets: [
-                {
-                    label: "Life",
-                    data: [],
-                    backgroundColor: [],
-                    borderColor: [
-                        "rgba(184, 14, 1, 1)",
-                        "rgba(255, 132, 29, 1)",
-                        "rgba(234, 212, 37, 1)",
-                        "rgba(13, 172, 64, 1)",
-                        "rgba(22, 125, 238, 1)",
-                        "rgba(93, 33, 143, 1)",
-                        "rgba(234, 94, 150, 1)"
-                    ],
-                    borderWidth: 1
-                }
-            ]
-        },
-        options: {
-            scale: {
-                display: false
-            },
-            legend: {
-                labels: {
-                    boxWidth: 30,
-                    padding: 5
-                }
-            },
-            responsive: true
+  var myChart = new Chart(ctx, {
+    type: "polarArea",
+    data: {
+      labels: [],
+      datasets: [
+        {
+          label: "Life",
+          data: [],
+          backgroundColor: [],
+          borderColor: [
+            "rgba(184, 14, 1, 1)",
+            "rgba(255, 132, 29, 1)",
+            "rgba(234, 212, 37, 1)",
+            "rgba(13, 172, 64, 1)",
+            "rgba(22, 125, 238, 1)",
+            "rgba(93, 33, 143, 1)",
+            "rgba(234, 94, 150, 1)"
+          ],
+          borderWidth: 1
         }
-    });
-
-    function addDatatToChart(value, label) {
-        myChart.data.datasets[0].data.push(value);
-        myChart.data.labels.push(label);
-
-        myChart.data.datasets[0].backgroundColor.push(
-            createGradient(colorPresets.shift())
-        );
-        myChart.update();
+      ]
+    },
+    options: {
+      scale: {
+        display: false
+      },
+      legend: {
+        labels: {
+          boxWidth: 30,
+          padding: 5
+        }
+      },
+      layout: {
+        padding: 20
+      },
+      responsive: true
     }
+  });
+
+  function addDatatToChart(value, label) {
+    myChart.data.datasets[0].data.push(value);
+    myChart.data.labels.push(label);
+
+    myChart.data.datasets[0].backgroundColor.push(
+      createGradient(colorPresets.shift())
+    );
+    myChart.update();
+  }
 })(jQuery);
--- public/partials/polar-chart-partial.php
+++ public/partials/polar-chart-partial.php
@@ -1,4 +1,4 @@
-........................<?php
+<?php
 /**
  * Template partial for polar chart questionnaire
  */
@@ -56,7 +56,6 @@
             </ul>
         </div>
 
-
         <div class="step hidden">
             <h1 class="center-text">Activity</h1>
             <h3 class="center-text"><?php echo get_option('pcq_heading_text', ''); ?></h3>
@@ -107,196 +106,196 @@
         </div>
 
         <div class="step hidden">
-            <h1 class="center-text">Cleansing</h1>
+            <h1 class="center-text">Self-Care</h1>
             <h3 class="center-text"><?php echo get_option('pcq_heading_text', ''); ?></h3>
             <p class="center-text">How would you rate this area of your life?</p>
 
             <ul class="select-list">
                 <li class="select-box">
-                    <input type="radio" name="cleansing" value="1">
+                    <input type="radio" name="self-care" value="1">
                     <span>1</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="cleansing" value="2">
+                    <input type="radio" name="self-care" value="2">
                     <span>2</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="cleansing" value="3">
+                    <input type="radio" name="self-care" value="3">
                     <span>3</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="cleansing" value="4">
+                    <input type="radio" name="self-care" value="4">
                     <span>4</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="cleansing" value="5">
+                    <input type="radio" name="self-care" value="5">
                     <span>5</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="cleansing" value="6">
+                    <input type="radio" name="self-care" value="6">
                     <span>6</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="cleansing" value="7">
+                    <input type="radio" name="self-care" value="7">
                     <span>7</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="cleansing" value="8">
+                    <input type="radio" name="self-care" value="8">
                     <span>8</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="cleansing" value="9">
+                    <input type="radio" name="self-care" value="9">
                     <span>9</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="cleansing" value="10">
+                    <input type="radio" name="self-care" value="10">
                     <span>10</span
                 ></li>
             </ul>
         </div>
 
         <div class="step hidden">
-            <h1 class="center-text">Love</h1>
+            <h1 class="center-text">Relationships</h1>
             <h3 class="center-text"><?php echo get_option('pcq_heading_text', ''); ?></h3>
             <p class="center-text">How would you rate this area of your life?</p>
 
             <ul class="select-list">
                 <li class="select-box">
-                    <input type="radio" name="love" value="1">
+                    <input type="radio" name="relationships" value="1">
                     <span>1</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="love" value="2">
+                    <input type="radio" name="relationships" value="2">
                     <span>2</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="love" value="3">
+                    <input type="radio" name="relationships" value="3">
                     <span>3</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="love" value="4">
+                    <input type="radio" name="relationships" value="4">
                     <span>4</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="love" value="5">
+                    <input type="radio" name="relationships" value="5">
                     <span>5</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="love" value="6">
+                    <input type="radio" name="relationships" value="6">
                     <span>6</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="love" value="7">
+                    <input type="radio" name="relationships" value="7">
                     <span>7</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="love" value="8">
+                    <input type="radio" name="relationships" value="8">
                     <span>8</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="love" value="9">
+                    <input type="radio" name="relationships" value="9">
                     <span>9</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="love" value="10">
+                    <input type="radio" name="relationships" value="10">
                     <span>10</span
                 ></li>
             </ul>
         </div>
 
         <div class="step hidden">
-            <h1 class="center-text">Relaxation</h1>
+            <h1 class="center-text">Adaptability</h1>
             <h3 class="center-text"><?php echo get_option('pcq_heading_text', ''); ?></h3>
             <p class="center-text">How would you rate this area of your life?</p>
 
             <ul class="select-list">
                 <li class="select-box">
-                    <input type="radio" name="relaxation" value="1">
+                    <input type="radio" name="adaptability" value="1">
                     <span>1</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="relaxation" value="2">
+                    <input type="radio" name="adaptability" value="2">
                     <span>2</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="relaxation" value="3">
+                    <input type="radio" name="adaptability" value="3">
                     <span>3</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="relaxation" value="4">
+                    <input type="radio" name="adaptability" value="4">
                     <span>4</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="relaxation" value="5">
+                    <input type="radio" name="adaptability" value="5">
                     <span>5</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="relaxation" value="6">
+                    <input type="radio" name="adaptability" value="6">
                     <span>6</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="relaxation" value="7">
+                    <input type="radio" name="adaptability" value="7">
                     <span>7</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="relaxation" value="8">
+                    <input type="radio" name="adaptability" value="8">
                     <span>8</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="relaxation" value="9">
+                    <input type="radio" name="adaptability" value="9">
                     <span>9</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="relaxation" value="10">
+                    <input type="radio" name="adaptability" value="10">
                     <span>10</span
                 ></li>
             </ul>
         </div>
 
         <div class="step hidden">
-            <h1 class="center-text">Service</h1>
+            <h1 class="center-text">Transformation</h1>
             <h3 class="center-text"><?php echo get_option('pcq_heading_text', ''); ?></h3>
             <p class="center-text">How would you rate this area of your life?</p>
 
             <ul class="select-list">
                 <li class="select-box">
-                    <input type="radio" name="service" value="1">
+                    <input type="radio" name="transformation" value="1">
                     <span>1</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="service" value="2">
+                    <input type="radio" name="transformation" value="2">
                     <span>2</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="service" value="3">
+                    <input type="radio" name="transformation" value="3">
                     <span>3</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="service" value="4">
+                    <input type="radio" name="transformation" value="4">
                     <span>4</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="service" value="5">
+                    <input type="radio" name="transformation" value="5">
                     <span>5</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="service" value="6">
+                    <input type="radio" name="transformation" value="6">
                     <span>6</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="service" value="7">
+                    <input type="radio" name="transformation" value="7">
                     <span>7</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="service" value="8">
+                    <input type="radio" name="transformation" value="8">
                     <span>8</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="service" value="9">
+                    <input type="radio" name="transformation" value="9">
                     <span>9</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="service" value="10">
+                    <input type="radio" name="transformation" value="10">
                     <span>10</span
                 ></li>
             </ul>
@@ -304,49 +303,49 @@
 
         <div class="step hidden">
 
-            <h1 class="center-text">Fun</h1>
+            <h1 class="center-text">Fulfillment</h1>
             <h3 class="center-text"><?php echo get_option('pcq_heading_text', ''); ?></h3>
             <p class="center-text">How would you rate this area of your life?</p>
 
             <ul class="select-list">
                 <li class="select-box">
-                    <input type="radio" name="fun" value="1">
+                    <input type="radio" name="fulfillment" value="1">
                     <span>1</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="fun" value="2">
+                    <input type="radio" name="fulfillment" value="2">
                     <span>2</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="fun" value="3">
+                    <input type="radio" name="fulfillment" value="3">
                     <span>3</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="fun" value="4">
+                    <input type="radio" name="fulfillment" value="4">
                     <span>4</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="fun" value="5">
+                    <input type="radio" name="fulfillment" value="5">
                     <span>5</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="fun" value="6">
+                    <input type="radio" name="fulfillment" value="6">
                     <span>6</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="fun" value="7">
+                    <input type="radio" name="fulfillment" value="7">
                     <span>7</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="fun" value="8">
+                    <input type="radio" name="fulfillment" value="8">
                     <span>8</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="fun" value="9">
+                    <input type="radio" name="fulfillment" value="9">
                     <span>9</span>
                 </li>
                 <li class="select-box">
-                    <input type="radio" name="fun" value="10">
+                    <input type="radio" name="fulfillment" value="10">
                     <span>10</span
                 ></li>
             </ul>
@@ -355,6 +354,8 @@
         <div class="step hidden">
             <h3 class="center-text font-heavy">Here are what your 7 pillars of wellbeing look like</h3>
             <h3 class="center-text"><?php echo get_option('pcq_ending_text', '');?></h3>
+
+            <button id="restart-butn" tclass="butn">Restart</button>
         </div>
     </form>
         </div>
